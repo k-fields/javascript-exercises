eee340739bffae73b4b9f6e9df9d8dec2f14f0ab
Replit user <>
2022-08-07 06:39:25 +0000

    Done exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+	let returnThis = '';
 
+	if (repeat < 0) return 'ERROR';
+	if (string === '') return '';
+	for(let i = 0; i < repeat; i++){
+		returnThis += string;
+	}
+	return returnThis;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
