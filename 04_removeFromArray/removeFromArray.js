7259303ef1b2546ae672796b60ce6e4905dca6d6
Replit user <>
2022-08-08 06:10:59 +0000

    Finishes exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+	const arr = arguments[0]; //the array received
+	const returnThis = arr.filter((x) => {
+		for (let i = 1; i < arguments.length; i++){
+			if (x === arguments[i]) return false;
+		}
+		return true;
+	})
 
+	return returnThis;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
