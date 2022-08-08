e38a806d43ab19a9d55cdb088175abe1dc66c0fb
Replit user <>
2022-08-08 06:10:36 +0000

    Finishes exercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	if (!string) return '';
+	let reverse = '';
+	for (let i = string.length-1; i >= 0; i--){
+		reverse += string[i];
+	}
+	return reverse;
 };
 
 // Do not edit below this line
