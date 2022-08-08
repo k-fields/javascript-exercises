5f875e53efb743c2d6c69985aeb1b63945971af9
Replit user <>
2022-08-08 06:25:22 +0000

    Finishes exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+	if  (x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number') return 'ERROR';
 
+	let sum = 0;
+
+	let larger = 0;
+	let smaller = 0;
+	/*
+	if (x > y){
+		larger = x;
+		smaller = y;
+	}
+	else
+	{
+		larger = y;
+		smaller = x;
+	}
+	*/
+
+	x > y ? (() => { larger = x; smaller = y;})():(() => {larger = y; smaller = x;})();
+
+	for (let i = smaller; i <= larger; i++){
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
