b879e05d023a2327215ba27ea85b82c5e62cb378
Replit user <>
2022-08-09 05:25:58 +0000

    Finishes exercise #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	let centuryYear = (year) => {
+		return year%100 == 0;
+	};
 
+	if (centuryYear(year)){
+		return year%400 == 0;
+	};
+
+	return year%4 == 0;	
 };
 
 // Do not edit below this line
