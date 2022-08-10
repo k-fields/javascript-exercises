2bbd5338ed3e4cdfa3c46de6ff527710aefca2db
Replit user <>
2022-08-10 05:48:12 +0000

    finishes exercise #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
+const sum = function(arr) {
+	let sum = 0;
+	if(!arr) return 0;
 	
+	for (let i = 0; i < arr.length; i++){
+		sum += arr[i];
+	}
+
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+	let mult = 1;
+	
 
+	for(let i = 0; i < arr.length;i++)
+		mult *= arr[i];
+
+	return mult;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 1 || num == 0) return 1;
+
+	return num * factorial(num-1);
 };
 
 // Do not edit below this line
