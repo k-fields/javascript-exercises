5097374e85bceb373d5a782a6f5d7424d69ce7f2
Replit user <>
2022-08-10 14:49:34 +0000

    Working on exercise #9 still

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+	//filter array for alphabetical characters
+	const arrayPhrase = phrase.split(' ').join('');
+	const filtered = arrayPhrase.filter((x) => {
+		return x.match(/[a-z]/gi);
+	});
 
-};
+	let len = Math.floor(filtered.length / 2) + 1;
 
+	for (let i = 0; i <= len; i++){
+		if (filtered[i] !== filtered[filtered.length - 1 - i])
+			return false;
+		return true;
+	}
+}
 // Do not edit below this line
 module.exports = palindromes;
