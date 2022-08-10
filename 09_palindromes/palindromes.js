12ad317befdeaf90445681c27314cbe9eb10dbb1
k-fields <caiocp3@hotmail.com>
2022-08-10 12:42:04 -0300

    Separates input in array removing some special symbols

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,16 @@
 const palindromes = function (phrase) {
 	//filter array for alphabetical characters
-	const arrayPhrase = phrase.split(' ').join('');
+	const arrayPhrase = phrase.split(/(,|\.|!|\?)/);
+	
 	const filtered = arrayPhrase.filter((x) => {
 		return x.match(/[a-z]/gi);
 	});
 
-	let len = Math.floor(filtered.length / 2) + 1;
-
-	for (let i = 0; i <= len; i++){
-		if (filtered[i] !== filtered[filtered.length - 1 - i])
-			return false;
-		return true;
-	}
+	
+	console.log(arrayPhrase);
+	console.log(filtered);
 }
+
+palindromes("Test, This, Shit");
 // Do not edit below this line
 module.exports = palindromes;
