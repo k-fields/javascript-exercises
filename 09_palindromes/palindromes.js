e8596e5b4812f6bbedbf0312558ef48afa0a6a52
k-fields <caiocp3@hotmail.com>
2022-08-11 11:05:40 -0300

    Filters received string for only alphabetical characters. No spaces.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,23 @@
 const palindromes = function (phrase) {
-	//filter array for alphabetical characters
-	const arrayPhrase = phrase.split(/(,|\.|!|\?)/);
-	
-	const filtered = arrayPhrase.filter((x) => {
-		return x.match(/[a-z]/gi);
-	});
+	const filtered = () => {
+		let stringy = '';
+		for (let i = 0; i < phrase.length; i++){
+			if (phrase[i].match(/[a-z]/i))
+				stringy += phrase[i];
+		}
+		return stringy;
+	}
 
-	
-	console.log(arrayPhrase);
-	console.log(filtered);
+	console.log(filtered());
 }
 
 palindromes("Test, This, Shit");
+/*
+const string = "My Little Pony";
+for (let i = 0; i < string.length; i++){
+	if (string[i].match(/[a-z]/i))
+		console.log(string[i]);
+}
+*/
 // Do not edit below this line
 module.exports = palindromes;
