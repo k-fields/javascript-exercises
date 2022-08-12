b9d3397cec6fd08dd6b6295df20cdef4d492abcf
k-fields <caiocp3@hotmail.com>
2022-08-12 01:02:18 -0300

    Finishes exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	const parsed = parseInt(num);
+	if (parsed < 0) return "OOPS";
+	let fibCurrent = 1;
+	let temp = 1;
+	let numBefore = 0;
+	//1 1 2 3 5 8 13 21
 
+	for (let i = 1; i < num; i++){
+		temp = numBefore;
+		numBefore = fibCurrent;
+		fibCurrent += temp;
+	}
+
+	return fibCurrent;
 };
 
 // Do not edit below this line
