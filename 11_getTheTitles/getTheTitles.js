f1f5b1c0d6979cad31d2270c1f9994f2a86d8a55
k-fields <caiocp3@hotmail.com>
2022-08-12 01:25:16 -0300

    Finishes exercise #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+	const titles = books.map((x) => {
+		return x['title'];
+	});
 
+	return titles;
 };
 
 // Do not edit below this line
