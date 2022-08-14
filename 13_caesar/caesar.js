131c0c8cb3481a88d8c8b27a01f68bfc6072b269
k-fields <caiocp3@hotmail.com>
2022-08-14 14:09:53 -0300

    Still working on Exercise # 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,21 @@
-const caesar = function() {
+const caesar = function(string, shifts) {
+    let returnThis = '';
 
+    for (let i = 0; i < string.length; i++){
+        let letterCode = string[i].charCodeAt();
+        if (string[i].match(/[\s!,\.]/i)){
+            returnThis += string[i];
+            continue;
+        }
+        if (string[i].match(/[A-Z]/)){ //maiúscula
+            returnThis += String.fromCharCode((letterCode + shifts) % 0x5A);
+            continue;
+        }
+
+        returnThis += String.fromCharCode((letterCode + shifts) % 0x7A);
+    }
+
+    return returnThis;
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.spec.js
+++ 13_caesar/caesar.spec.js
@@ -1,12 +1,12 @@
 const caesar = require('./caesar')
 
-test('works with single letters', () => {
+test.skip('works with single letters', () => {
   expect(caesar('A', 1)).toBe('B');
 });
 test.skip('works with words', () => {
   expect(caesar('Aaa', 1)).toBe('Bbb');
 });
-test.skip('works with phrases', () => {
+test('works with phrases', () => {
   expect(caesar('Hello, World!', 5)).toBe('Mjqqt, Btwqi!');
 });
 test.skip('works with negative shift', () => {
